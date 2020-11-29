263eb5a4085dd8747a8d7c21b82c91757cf99bf7
Marin Tsanov <btg.tsanov.marin@gmail.com>
2020-11-29 21:24:11 +0200

    All repositories are now children of JpaRepository Instead of CrudRepository

--- src/main/java/com/mtsan/techstore/repositories/ProductRepository.java
+++ src/main/java/com/mtsan/techstore/repositories/ProductRepository.java
@@ -1,8 +1,8 @@
 package com.mtsan.techstore.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.mtsan.techstore.entities.Product;
 
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
---
+++ src/main/java/com/mtsan/techstore/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.mtsan.techstore.repositories;
+
+import com.mtsan.techstore.Rank;
+import com.mtsan.techstore.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	@Query(value = "SELECT * FROM users WHERE rank = ?1", nativeQuery = true)
+	List<User> getUserByRank(String rank);
+
+}
