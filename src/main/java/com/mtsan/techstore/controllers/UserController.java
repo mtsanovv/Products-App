fe1b1e666af765d9b9036c384afc4f3800180edc
Marin Tsanov <btg.tsanov.marin@gmail.com>
2020-12-01 23:48:07 +0200

    Renamed DashboardController to UserController Basically all it does is show the user data after a successful login

--- src/main/java/com/mtsan/techstore/controllers/UserController.java
+++ src/main/java/com/mtsan/techstore/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.mtsan.techstore.controllers;
 
-import com.mtsan.techstore.Rank;
 import com.mtsan.techstore.entities.User;
 import com.mtsan.techstore.exceptions.TechstoreDataException;
 import com.mtsan.techstore.repositories.UserRepository;
@@ -8,14 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DashboardController {
+public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
